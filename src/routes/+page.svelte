3c68220bd4575ccfb1aa7a47bb3c710d22cb26d9
unknown <nikhilmelkani28@gmail.com>
2024-05-17 20:20:34 +0530

    deeznuts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,65 @@
 <script lang="ts">
-    import Main from "./Profile.svelte";
-    import {onMount } from "svelte";
+  import Main from "./Profile.svelte";
+  import { onMount } from "svelte";
 
-    
-    const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-    const REDIRECT_URI = "http://localhost:5173";
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-    const SCOPES = "user-read-private user-read-email user-top-read";
-    function login(){
-        const LOGIN_URI = new URLSearchParams({
-            client_id:CLIENT_ID,
-            response_type:'token',
-            redirect_uri:REDIRECT_URI,
-            scope:SCOPES
-        });
-        window.location.href = `${AUTH_ENDPOINT}?${LOGIN_URI}`;
-    }
-    let isLoggedIn = false;
-    let urlParams: URLSearchParams | null = null; // Declare urlParams variable
-    // Check if the page has been redirected from Spotify after successful login
-    onMount(() => {
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:5173";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const SCOPES = "user-read-private user-read-email user-top-read";
+  function login() {
+    const LOGIN_URI = new URLSearchParams({
+      client_id: CLIENT_ID,
+      response_type: "token",
+      redirect_uri: REDIRECT_URI,
+      scope: SCOPES,
+    });
+    window.location.href = `${AUTH_ENDPOINT}?${LOGIN_URI}`;
+  }
+  let isLoggedIn = false;
+  let urlParams: URLSearchParams | null = null; // Declare urlParams variable
+  // Check if the page has been redirected from Spotify after successful login
+  onMount(() => {
     urlParams = new URLSearchParams(window.location.hash.substring(1));
-    if (urlParams?.has('access_token')) {
-        isLoggedIn = true;
+    if (urlParams?.has("access_token")) {
+      isLoggedIn = true;
     }
-});
-
+  });
 </script>
+
 {#if isLoggedIn}
-    <!-- Content to show after successful login -->
-    <Main accessToken="{urlParams?.get('access_token')}"/>
-    <!-- Add more content or components here -->
+  <!-- Content to show after successful login -->
+  <Main accessToken={urlParams?.get("access_token")} />
+  <!-- Add more content or components here -->
 {:else}
-    <!-- Login button -->
+  <!-- Login button -->
+  <div class="center">
     <h1>Spotify Profile</h1>
     <button type="button" on:click={login}>Log In</button>
+  </div>
+  
 {/if}
+
+<style>
+      /* Centering styles */
+  .center {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* Set height to occupy full viewport height */
+    font-family: "Gotham";
+  }
+
+  /* Optional: Add styling to the button */
+  button {
+    margin-top: 10px;
+    padding: 10px 20px;
+    background-color: #1db954;
+    color: white;
+    border: none;
+    border-radius: 25px;
+    cursor: pointer;
+    font-family: "Gotham Medium";
+    font-size: larger;
+  }
+</style>
--- src/routes/Profile.svelte
+++ src/routes/Profile.svelte
@@ -8,8 +8,6 @@
   let userInfo: SpotifyUser | null = null;
   let topTracks: SpotifyTrack[] = [];
   let topArtists: SpotifyArtist[] = [];
-  // Simulate fetching user info
-  
 
   onMount(async () => {
     if (accessToken) {
@@ -30,8 +28,8 @@
     localStorage.removeItem("access_token");
     window.location.href = "/";
   }
-
 </script>
+
 <main>
   {#if userInfo}
     <div class="profile">
@@ -51,12 +49,24 @@
         {#each topTracks.slice(0, 5) as track}
           <li class="track-info">
             {#if track.album.images[2]?.url}
-              <img src={track.album.images[2].url} alt="Album cover" class="album-cover" />
+              <img
+                src={track.album.images[2].url}
+                alt="Album cover"
+                class="album-cover"
+              />
             {/if}
             <div class="track-details">
-              <a href={track.externalUrls.spotify} target="_blank" class="track-name">{track.name}</a>
+              <a
+                href={track.externalUrls.spotify}
+                target="_blank"
+                class="track-name">{track.name}</a
+              >
               {#each track.artists as artist}
-                <a href={artist.externalUrls.spotify} target="_blank" class="artist-name">{artist.name}</a>
+                <a
+                  href={artist.externalUrls.spotify}
+                  target="_blank"
+                  class="artist-name">{artist.name}</a
+                >
               {/each}
             </div>
           </li>
@@ -70,144 +80,152 @@
         {#each topArtists.slice(0, 5) as artist}
           <li class="artist-info">
             {#if artist.images[2]?.url}
-              <img src={artist.images[2].url} alt="{artist.name}" class="artist-image" />
+              <img
+                src={artist.images[2].url}
+                alt={artist.name}
+                class="artist-image"
+              />
             {/if}
-            <a href={artist.externalUrls.spotify} target="_blank" class="artist-name top-artist-name">{artist.name}</a>
+            <a
+              href={artist.externalUrls.spotify}
+              target="_blank"
+              class="artist-name top-artist-name">{artist.name}</a
+            >
           </li>
         {/each}
       </ul>
     </div>
   </div>
 </main>
+
 <style>
-/* Main styles */
-main {
-  padding: 10px 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  box-sizing: border-box;
-  overflow: hidden;
-}
+  /* Main styles */
+  main {
+    padding: 10px 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    box-sizing: border-box;
+    overflow: hidden;
+  }
 
-/* Profile styles */
-.profile {
-  text-align: center;
-  margin-bottom: 20px;
-}
+  /* Profile styles */
+  .profile {
+    text-align: center;
+    margin-bottom: 20px;
+  }
 
-.profile img {
-  border-radius: 50%;
-  width: 125px;
-  height: 125px;
-}
+  .profile img {
+    border-radius: 50%;
+    width: 125px;
+    height: 125px;
+  }
 
-.profile h1 {
-  font-size: 1.5em;
-  margin: 10px 0;
-}
+  .profile h1 {
+    font-size: 1.5em;
+    margin: 10px 0;
+  }
 
-.profile button {
-  padding: 8px 16px;
-  background-color: #1db954;
-  border: none;
-  border-radius: 25px;
-  color: #fff;
-  font-weight: 700;
-  cursor: pointer;
-  font-family: "Gotham Light";
-  font-size: medium;
-}
+  .profile button {
+    padding: 8px 16px;
+    background-color: #1db954;
+    border: none;
+    border-radius: 25px;
+    color: #fff;
+    font-weight: 700;
+    cursor: pointer;
+    font-family: "Gotham Light";
+    font-size: medium;
+  }
 
-/* Divider styles */
-.divider {
-  width: 80%;
-  height: 1px;
-  background-color: grey;
-  margin: 10px 0;
-}
+  /* Divider styles */
+  .divider {
+    width: 80%;
+    height: 1px;
+    background-color: grey;
+    margin: 10px 0;
+  }
 
-/* Content styles */
-.content {
-  display: flex;
-  justify-content: space-between;
-  width: 80%;
-  height: calc(100vh - 200px);
-  box-sizing: border-box;
-}
+  /* Content styles */
+  .content {
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    height: calc(100vh - 200px);
+    box-sizing: border-box;
+  }
 
-/* Tracks and Artists styles */
-.tracks,
-.artists {
-  width: 48%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
+  /* Tracks and Artists styles */
+  .tracks,
+  .artists {
+    width: 48%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
 
-.tracks h2,
-.artists h2 {
-  font-size: 1.5em; /* Increase the size of the headings */
-  font-weight: bold;
-  margin-bottom: 10px;
-}
+  .tracks h2,
+  .artists h2 {
+    font-size: 1.5em; /* Increase the size of the headings */
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
 
-/* Album cover and Artist image styles */
-.album-cover,
-.artist-image {
-  width: 50px;
-  height: 50px;
-  border-radius: 8px;
-  margin-right: 8px;
-}
+  /* Album cover and Artist image styles */
+  .album-cover,
+  .artist-image {
+    width: 50px;
+    height: 50px;
+    border-radius: 8px;
+    margin-right: 8px;
+  }
 
-/* Track and Artist info styles */
-.track-info,
-.artist-info {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
+  /* Track and Artist info styles */
+  .track-info,
+  .artist-info {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
 
-/* Track details styles */
-.track-details {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start; /* Align text within track-details to the start */
-}
+  /* Track details styles */
+  .track-details {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start; /* Align text within track-details to the start */
+  }
 
-.track-name {
-  font-size: 1.2rem;
-  font-weight: bold;
-  text-align: left; /* Align the track name to the left */
-  margin-bottom: 2px;
-}
+  .track-name {
+    font-size: 1.2rem;
+    font-weight: bold;
+    text-align: left; /* Align the track name to the left */
+    margin-bottom: 2px;
+  }
 
-.artist-name {
-  font-size: 1em;
-  text-align: left; /* Align the artist name to the left */
-}
+  .artist-name {
+    font-size: 1em;
+    text-align: left; /* Align the artist name to the left */
+  }
 
-.top-artist-name {
-  font-size: 1.2rem;
-  font-weight: bold;
-  margin-left: 8px;
-}
+  .top-artist-name {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-left: 8px;
+  }
 
-/* Remove bullet points from lists */
-.tracks ul,
-.artists ul {
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-}
+  /* Remove bullet points from lists */
+  .tracks ul,
+  .artists ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
 
-/* Remove underlines from links */
-a {
-  text-decoration: none;
-  color: inherit;
-}
-
+  /* Remove underlines from links */
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 </style>
